4d4735955a08d24ad0eb88e60f311a94b195c10f
Anish Kumar <kumaranish2605@gmail.com>
2026-06-11 15:44:17 +0530

    Initial Commit

---
+++ src/main/java/com/example/Saloon/Controller/AppointmentController.java
@@ -0,0 +1,10 @@
+package com.example.Saloon.Controller;
+
+public class AppointmentController {
+    void Schedule()
+    {
+//        System.out.println("Appointment Schedule");
+//        System.out.printf("1. Book an appointment\n2. View appointments\n3. Cancel an appointment\n4. Exit\n");
+
+    }
+}
---
+++ src/test/java/com/example/Saloon/SaloonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Saloon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaloonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
